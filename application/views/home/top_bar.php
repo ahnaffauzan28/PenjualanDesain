70cdc4ba27448e4d66e61ce42de9e79465447be1
ahnaffauzan28 <mohammadahnaffauzan@gmail.com>
2023-12-18 01:51:30 +0700

    first commit

---
+++ application/views/home/top_bar.php
@@ -0,0 +1,19 @@
+    <div class="border-bottom top-bar py-2">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <p class="mb-0">
+          
+            </p>
+          </div>
+          <div class="col-md-6">
+            <ul class="social-media">
+              <li><a href="#" class="p-2"><span class="icon-facebook"></span></a></li>
+              <li><a href="#" class="p-2"><span class="icon-twitter"></span></a></li>
+              <li><a href="#" class="p-2"><span class="icon-instagram"></span></a></li>
+              <li><a href="#" class="p-2"><span class="icon-linkedin"></span></a></li>
+            </ul>
+          </div>
+        </div>
+      </div> 
+    </div>
